ad91f5534f95e40f084df8420a27f05a29c1b7fd
Rafael Nuansa <iandrewnew@gmail.com>
2024-06-25 14:22:12 +0700

    godotenv

--- models/setup.go
+++ models/setup.go
@@ -1,6 +1,10 @@
 package models
 
 import (
+	"fmt"
+	"os"
+
+	"github.com/joho/godotenv"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
@@ -8,12 +12,26 @@ import (
 var DB *gorm.DB
 
 func ConnectDatabase() {
-	database, err := gorm.Open(mysql.Open("root:@tcp(127.0.0.1:3306)/db_go_api"), &gorm.Config{})
+	// Muat file .env
+	err := godotenv.Load()
+	if err != nil {
+		panic("Error loading .env file")
+	}
+
+	// Ambil variabel-variabel dari lingkungan
+	hostname := os.Getenv("HOSTNAME")
+	dbport := os.Getenv("DBPORT")
+	dbusername := os.Getenv("DBUSERNAME")
+	dbname := os.Getenv("DBNAME")
+	dbpassword := os.Getenv("DBPASSWORD")
+
+	// Buat string koneksi
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s", dbusername, dbpassword, hostname, dbport, dbname)
+	database, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	if err != nil {
 		panic("failed to connect database")
 	}
 
 	database.AutoMigrate(&Post{})
-
 	DB = database
 }
